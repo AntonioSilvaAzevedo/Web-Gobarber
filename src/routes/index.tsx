a213c90d70ee11106e367601a1bece023d2d72d4
Antonio Carlos <antonie.dev@gmail.com>
2020-08-10 22:55:37 -0300

    comitão da massa

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,16 @@ import Route from './Route';
 
 import SingIn from '../pages/SingIn';
 import SingUp from '../pages/SingUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SingIn} />
     <Route path="/singup" component={SingUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
