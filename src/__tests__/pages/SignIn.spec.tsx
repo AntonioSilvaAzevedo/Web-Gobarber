80aa769522bcf156659844f8739bb0afaa423013
Antonio Carlos <antonie.dev@gmail.com>
2020-08-27 13:44:55 -0300

    test add yat

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SingIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(), // função vazia, sem retorno
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
 
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
+    const { getAllByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getAllByPlaceholderText('E-mail');
+    const passwordField = getAllByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@example.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
